a8b061d5491a7df6b5fbfe50fd6db6cab7f30028
Hearmic <hearmc@gmail.com>
2025-12-13 02:56:01 +0000

    Did set up the ssl certificates and am ready for ads

--- app/main/urls.py
+++ app/main/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from main import views
+from django.views.generic.base import RedirectView
+from django.contrib.staticfiles.storage import staticfiles_storage
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('calculate/', views.calculate_prediction, name='calculate_prediction'),
     path('health/', views.health_check, name='health_check'),
+    path('ads.txt', RedirectView.as_view(url=staticfiles_storage.url('main/ads.txt'))),
 ]
